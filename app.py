306b22e0243613c12cd11b2adab6403d50efee1f
Ash1906 <jainashmika.1906@gmail.com>
2021-05-28 22:33:50 +0530

    updated for jokes

--- app.py
+++ app.py
@@ -92,10 +92,12 @@ def respond():
         """
         # send the welcoming message
         bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+
+
      elif text == "/bore":
          re = requests.get("https://official-joke-api.appspot.com/random_joke")
-#print(re.status_code)
-#print(re.text)
+        #print(re.status_code)
+        #print(re.text) 
          dict = re.json()
          punch = dict['setup'] +" " +  dict['punchline']
 
